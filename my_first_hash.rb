3cd8dc26eeb3f24b7de8c3d452531ec072d2f855
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-15 22:57:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest["pearl necklace"] = 1
+shipping_manifest["gun powder"] = 4
 shipping_manifest
 
 end
