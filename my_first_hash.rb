7a8c45894876d5fa899a110b7bf64b418bb26ef0
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-15 23:14:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,8 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}	 
   
-shipping_manifest["muskets"] = "2"
-shipping_manifest["muskets"]
+#shipping_manifest["muskets"] = "2"
+#shipping_manifest["muskets"]
 hash = {"muskets" => "2"}
 hash["gun powder"] = "4"
 end	
